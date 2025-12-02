62d015fe6abea0e6ad9778fc03af9ef636c39a04
Alexander Torres <alexanderdtorres84@gmail.com>
2025-12-02 10:59:22 -0800

    gerer

---
+++ app/src/main/java/com/example/loginandregistration/RegistrationActivity.kt
@@ -0,0 +1,25 @@
+package com.example.loginandregistration
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.loginandregistration.databinding.ActivityRegistrationBinding
+
+class RegistrationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegistrationBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityRegistrationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.layoutRegistration) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
